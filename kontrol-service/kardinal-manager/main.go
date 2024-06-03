34dd243aac49884ee1323b9b568fbf5d42fc4c5c
Tedi Mitiku <tedi.m52@gmail.com>
2024-06-03 18:16:53 -0400

    apply config via istio golang api

--- kontrol-service/kardinal-manager/main.go
+++ kontrol-service/kardinal-manager/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"path/filepath"
+	"time"
 
+	istio "istio.io/api/networking/v1alpha3"
+	istioconfig "istio.io/client-go/pkg/applyconfiguration/networking/v1alpha3"
+	istioclient "istio.io/client-go/pkg/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -46,4 +51,120 @@ func main() {
 	for _, pod := range pods.Items {
 		fmt.Printf("Pod Name: %s\n", pod.Name)
 	}
+
+	// create ist io client
+
+	ic, err := istioclient.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create istio client: %s", err)
+	}
+
+	namespace := ""
+	vsClient := ic.NetworkingV1alpha3().VirtualServices(namespace)
+	drClient := ic.NetworkingV1alpha3().DestinationRules(namespace)
+
+	// Test VirtualServices
+	vsList, err := vsClient.List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to retrieve VirtualServices in namespace: %s with error:\n%s", namespace, err)
+	}
+	for i := range vsList.Items {
+		vs := vsList.Items[i]
+		log.Printf("Index: %d VirtualService Hosts: %+v\n", i, vs.Spec.GetHosts())
+	}
+
+	// Test DestinationRules
+	drList, err := drClient.List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get DestinationRule in %s namespace: %s", namespace, err)
+	}
+	for i := range drList.Items {
+		dr := drList.Items[i]
+		log.Printf("Index: %d DestinationRule Host: %+v\n", i, dr.Spec.GetHost())
+	}
+
+	// turn this command into a programmatic k8s api call:
+	// kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
+	// creates Virtual Services for each service that routes all traffic to one subset of that service
+	fmt.Printf("Attempting to apply routing rule ...")
+	//apiVersion: networking.istio.io/v1alpha3
+	//kind: VirtualService
+	//metadata:
+	//name: reviews
+	//spec:
+	//hosts:
+	//	- reviews
+	//http:
+	//	- match:
+	//	- headers:
+	//	end-user:
+	//exact: jason
+	//route:
+	//	- destination:
+	//host: reviews
+	//subset: v2
+	//	- route:
+	//	- destination:
+	//host: reviews
+	//subset: v1
+	vs := istio.VirtualService{
+		Hosts: []string{"reviews"},
+		Http: []*istio.HTTPRoute{
+			{
+				Route: []*istio.HTTPRouteDestination{
+					{
+						Destination: &istio.Destination{
+							Host:   "reviews",
+							Subset: "v1",
+						},
+					},
+				},
+			},
+			{
+				Match: []*istio.HTTPMatchRequest{
+					{
+						Uri: &istio.StringMatch{
+							MatchType: &istio.StringMatch_Prefix{
+								Prefix: "/productpage/v2",
+							},
+						},
+					},
+				},
+				Route: []*istio.HTTPRouteDestination{
+					{
+						Destination: &istio.Destination{
+							Host:   "reviews",
+							Subset: "v2",
+						},
+					},
+				},
+			},
+		},
+	}
+	virtualServiceConfig := istioconfig.VirtualService("reviews", "")
+	virtualServiceConfig.
+		WithAPIVersion("networking.istio.io/v1alpha3").
+		WithKind("VirtualService").
+		WithAnnotations(map[string]string{"name": "reviews"}).
+		WithSpec(vs)
+	_, err = vsClient.Apply(ctx, virtualServiceConfig, metav1.ApplyOptions{})
+	if err != nil {
+		fmt.Printf("An error occurred applying virtual service config: %s", err)
+	}
+	fmt.Println("Virtual Service configured successfully.")
+
+	// Test Gateway
+	//gwList, err := ic.NetworkingV1alpha3().Gateways(namespace).List(context.TODO(), metav1.ListOptions{})
+	//if err != nil {
+	//	log.Fatalf("Failed to get Gateway in %s namespace: %s", namespace, err)
+	//}
+	//
+	//for i := range gwList.Items {
+	//	gw := gwList.Items[i]
+	//	for _, s := range gw.Spec.GetServers() {
+	//		log.Printf("Index: %d Gateway servers: %+v\n", i, s)
+	//	}
+	//}
+
+	time.Sleep(100000 * time.Minute)
 }
