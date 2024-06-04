7e296f0326264dbdd1c2f4e4019d799c4bf84f10
Tedi Mitiku <tedi.m52@gmail.com>
2024-06-04 09:47:14 -0400

    use update api to update traffic routing

--- kontrol-service/kardinal-manager/main.go
+++ kontrol-service/kardinal-manager/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	istio "istio.io/api/networking/v1alpha3"
-	istioconfig "istio.io/client-go/pkg/applyconfiguration/networking/v1alpha3"
+	istionetworking "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	istioclient "istio.io/client-go/pkg/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -58,7 +58,7 @@ func main() {
 		log.Fatalf("Failed to create istio client: %s", err)
 	}
 
-	namespace := ""
+	namespace := "default"
 	vsClient := ic.NetworkingV1alpha3().VirtualServices(namespace)
 	drClient := ic.NetworkingV1alpha3().DestinationRules(namespace)
 
@@ -67,8 +67,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to retrieve VirtualServices in namespace: %s with error:\n%s", namespace, err)
 	}
+	var reviewsVirtualService *istionetworking.VirtualService
 	for i := range vsList.Items {
 		vs := vsList.Items[i]
+		if vs.Name == "reviews" {
+			reviewsVirtualService = vs
+		}
 		log.Printf("Index: %d VirtualService Hosts: %+v\n", i, vs.Spec.GetHosts())
 	}
 
@@ -86,84 +90,31 @@ func main() {
 	// kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
 	// creates Virtual Services for each service that routes all traffic to one subset of that service
 	fmt.Printf("Attempting to apply routing rule ...\n")
-	//apiVersion: networking.istio.io/v1alpha3
-	//kind: VirtualService
-	//metadata:
-	//name: reviews
-	//spec:
-	//hosts:
-	//	- reviews
-	//http:
-	//	- match:
-	//	- headers:
-	//	end-user:
-	//exact: jason
-	//route:
-	//	- destination:
-	//host: reviews
-	//subset: v2
-	//	- route:
-	//	- destination:
-	//host: reviews
-	//subset: v1
-	vs := istio.VirtualService{
-		Hosts: []string{"reviews"},
-		Http: []*istio.HTTPRoute{
+	newReviewsRoute := istio.HTTPRoute{
+		Match: []*istio.HTTPMatchRequest{
 			{
-				Route: []*istio.HTTPRouteDestination{
-					{
-						Destination: &istio.Destination{
-							Host:   "reviews",
-							Subset: "v1",
-						},
-					},
-				},
-			},
-			{
-				Match: []*istio.HTTPMatchRequest{
-					{
-						Uri: &istio.StringMatch{
-							MatchType: &istio.StringMatch_Prefix{
-								Prefix: "/productpage/v2",
-							},
-						},
-					},
-				},
-				Route: []*istio.HTTPRouteDestination{
-					{
-						Destination: &istio.Destination{
-							Host:   "reviews",
-							Subset: "v2",
-						},
+				Uri: &istio.StringMatch{
+					MatchType: &istio.StringMatch_Prefix{
+						Prefix: "/reviews/0",
 					},
 				},
 			},
 		},
+		Route: []*istio.HTTPRouteDestination{
+			{
+				Destination: &istio.Destination{
+					Host:   "reviews",
+					Subset: "v2",
+				},
+			},
+		},
 	}
-	virtualServiceConfig := istioconfig.VirtualService("reviews", "")
-	virtualServiceConfig.
-		WithAPIVersion("networking.istio.io/v1alpha3").
-		WithKind("VirtualService").
-		WithAnnotations(map[string]string{"name": "reviews"}).
-		WithSpec(vs)
-	_, err = vsClient.Apply(ctx, virtualServiceConfig, metav1.ApplyOptions{})
+	reviewsVirtualService.Spec.Http = append([]*istio.HTTPRoute{&newReviewsRoute}, reviewsVirtualService.Spec.Http...)
+	reviewsVirtualService, err = vsClient.Update(ctx, reviewsVirtualService, metav1.UpdateOptions{})
 	if err != nil {
-		fmt.Printf("An error occurred applying virtual service config: %s", err)
+		log.Fatalf("An error occurred updating reviews virtual service: %v\n", err.Error())
 	}
-	fmt.Println("Virtual Service configured successfully.")
-
-	// Test Gateway
-	//gwList, err := ic.NetworkingV1alpha3().Gateways(namespace).List(context.TODO(), metav1.ListOptions{})
-	//if err != nil {
-	//	log.Fatalf("Failed to get Gateway in %s namespace: %s", namespace, err)
-	//}
-	//
-	//for i := range gwList.Items {
-	//	gw := gwList.Items[i]
-	//	for _, s := range gw.Spec.GetServers() {
-	//		log.Printf("Index: %d Gateway servers: %+v\n", i, s)
-	//	}
-	//}
+	fmt.Println("Reviews virtual service configured successfully.")
 
 	time.Sleep(100000 * time.Minute)
 }
