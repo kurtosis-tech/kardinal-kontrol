a9c4e2834b2e8abf5535744bec89c3f01861a463
lostbean <talktoedgar@gmail.com>
2024-06-19 10:13:37 -0300

    turn templates into functions

--- kloud-kontrol/engine/template/dev-flow.go
+++ kloud-kontrol/engine/template/dev-flow.go
@@ -1,227 +1,259 @@
 package template
 
 import (
+	"fmt"
+
+	composetypes "github.com/compose-spec/compose-go/types"
+	"github.com/samber/lo"
 	"istio.io/api/networking/v1alpha3"
 	istioclient "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	apps "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-// Define the Service
-var redisService = &v1.Service{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "v1",
-		Kind:       "Service",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "redis-prod",
-		Namespace: "voting-app",
-		Labels: map[string]string{
-			"app":     "redis-prod",
-			"version": "v1",
-		},
-	},
-	Spec: v1.ServiceSpec{
-		Ports: []v1.ServicePort{
-			{
-				Name:       "tcp-redis",
-				Port:       6379,
-				Protocol:   v1.ProtocolTCP,
-				TargetPort: intstr.FromInt(6379),
-			},
-		},
-		Selector: map[string]string{
-			"app": "redis-prod",
-		},
-	},
+type Traffic struct {
+	HasMirroring     bool
+	MirrorPercentage uint
+	MirrorToVersion  string
+	ExternalHostname string
+	GatewayName      string
 }
 
-var vol25pct = intstr.FromString("25%")
+// TODO: Needs to: 1) Validate/restrict version and name, 2) assume just on port on TCP
+// TODO: Remove dup ports and name
+type ServiceSpec struct {
+	Version    string
+	Name       string
+	Port       int32
+	TargetPort int32
+	Config     composetypes.ServiceConfig
+}
 
-// Define the Deployment
-var redisDeployment = &apps.Deployment{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "v1",
-		Kind:       "Deployment",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "redis-prod-v1",
-		Namespace: "voting-app",
-		Labels: map[string]string{
-			"app":     "redis-prod",
-			"version": "v1",
+type NamespaceSpec struct {
+	Name string
+}
+
+// Define the Service
+func Service(serviceSpec ServiceSpec, namespaceSpec NamespaceSpec) v1.Service {
+	return v1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
 		},
-	},
-	Spec: apps.DeploymentSpec{
-		Replicas: int32Ptr(1),
-		Selector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{
-				"app":     "redis-prod",
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceSpec.Name,
+			Namespace: namespaceSpec.Name,
+			Labels: map[string]string{
+				"app":     serviceSpec.Name,
+				"version": serviceSpec.Version,
+			},
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Name:       fmt.Sprintf("tcp-%s", serviceSpec.Name),
+					Port:       serviceSpec.Port,
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(int(serviceSpec.TargetPort)),
+				},
+			},
+			Selector: map[string]string{
+				"app": serviceSpec.Name,
+			},
+		},
+	}
+}
+
+func Deployment(serviceSpec ServiceSpec, namespaceSpec NamespaceSpec) apps.Deployment {
+	vol25pct := intstr.FromString("25%")
+	numReplicas := int32(1)
+	configPorts := serviceSpec.Config.Ports
+
+	containerPorts := lo.Map(configPorts, func(port composetypes.ServicePortConfig, _ int) v1.ContainerPort {
+		return v1.ContainerPort{
+			Name:          fmt.Sprintf("%s-%d", port.Protocol, port.Target),
+			ContainerPort: int32(port.Target),
+			Protocol:      v1.Protocol(port.Protocol),
+		}
+	})
+
+	envVars := lo.MapToSlice(serviceSpec.Config.Environment, func(key string, value *string) v1.EnvVar {
+		return v1.EnvVar{
+			Name:  key,
+			Value: *value,
+		}
+	})
+
+	return apps.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s", serviceSpec.Name, serviceSpec.Version),
+			Namespace: namespaceSpec.Name,
+			Labels: map[string]string{
+				"app":     serviceSpec.Name,
+				"version": serviceSpec.Version,
+			},
+		},
+		Spec: apps.DeploymentSpec{
+			Replicas: int32Ptr(numReplicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app":     serviceSpec.Name,
+					"version": serviceSpec.Version,
+				},
+			},
+			Strategy: apps.DeploymentStrategy{
+				Type: apps.RollingUpdateDeploymentStrategyType,
+				RollingUpdate: &apps.RollingUpdateDeployment{
+					MaxSurge:       &vol25pct,
+					MaxUnavailable: &vol25pct,
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"sidecar.istio.io/inject": "true",
+					},
+					Labels: map[string]string{
+						"app":     serviceSpec.Name,
+						"version": serviceSpec.Version,
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  serviceSpec.Config.ContainerName,
+							Image: serviceSpec.Config.Image,
+							Env:   envVars,
+							Ports: containerPorts,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// TODO: split prod and and dev flows
+func Gateway() istioclient.Gateway {
+	return istioclient.Gateway{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "networking.istio.io/v1alpha3",
+			Kind:       "Gateway",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "voting-app",
+			Namespace: "voting-app",
+			Labels: map[string]string{
+				"app":     "voting-app",
 				"version": "v1",
 			},
 		},
-		Strategy: apps.DeploymentStrategy{
-			Type: apps.RollingUpdateDeploymentStrategyType,
-			RollingUpdate: &apps.RollingUpdateDeployment{
-				MaxSurge:       &vol25pct,
-				MaxUnavailable: &vol25pct,
+		Spec: v1alpha3.Gateway{
+			Selector: map[string]string{
+				"istio": "ingressgateway",
 			},
-		},
-		Template: v1.PodTemplateSpec{
-			ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{
-					"sidecar.istio.io/inject": "true",
-				},
-				Labels: map[string]string{
-					"app":     "redis-prod",
-					"version": "v1",
-				},
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					{
-						Name:  "redis-prod",
-						Image: "bitnami/redis:6.0.8",
-						Env: []v1.EnvVar{
-							{
-								Name:  "ALLOW_EMPTY_PASSWORD",
-								Value: "yes",
-							},
-							{
-								Name:  "REDIS_PORT_NUMBER",
-								Value: "6379",
-							},
-						},
-						Resources: v1.ResourceRequirements{
-							Requests: v1.ResourceList{
-								v1.ResourceCPU:    resource.MustParse("100m"),
-								v1.ResourceMemory: resource.MustParse("128Mi"),
-							},
-							Limits: v1.ResourceList{
-								v1.ResourceCPU:    resource.MustParse("250m"),
-								v1.ResourceMemory: resource.MustParse("256Mi"),
-							},
-						},
-						Ports: []v1.ContainerPort{
-							{
-								ContainerPort: 6379,
-								Name:          "redis",
-							},
-						},
+			Servers: []*v1alpha3.Server{
+				{
+					Port: &v1alpha3.Port{
+						Number:   80,
+						Name:     "http",
+						Protocol: "HTTP",
+					},
+					Hosts: []string{
+						"voting-app.localhost",
+						"dev.voting-app.localhost",
 					},
 				},
 			},
 		},
-	},
-}
-
-// Define the Gateway
-var votingAppGateway = &istioclient.Gateway{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "networking.istio.io/v1alpha3",
-		Kind:       "Gateway",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "voting-app",
-		Namespace: "voting-app",
-		Labels: map[string]string{
-			"app":     "voting-app",
-			"version": "v1",
-		},
-	},
-	Spec: v1alpha3.Gateway{
-		Selector: map[string]string{
-			"istio": "ingressgateway",
-		},
-		Servers: []*v1alpha3.Server{
-			{
-				Port: &v1alpha3.Port{
-					Number:   80,
-					Name:     "http",
-					Protocol: "HTTP",
-				},
-				Hosts: []string{
-					"voting-app.localhost",
-					"dev.voting-app.localhost",
-				},
-			},
-		},
-	},
+	}
 }
 
 // Define the VirtualService
-var votingAppVirtualService = &istioclient.VirtualService{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "networking.istio.io/v1alpha3",
-		Kind:       "VirtualService",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "voting-app-ui",
-		Namespace: "voting-app",
-	},
-	Spec: v1alpha3.VirtualService{
-		Hosts: []string{
-			"voting-app.localhost",
-		},
-		Gateways: []string{
-			"voting-app",
-		},
-		Http: []*v1alpha3.HTTPRoute{
+func FrontendVirtualService(serviceSpec ServiceSpec, nameNamespaceSpec NamespaceSpec, traffic Traffic) istioclient.VirtualService {
+	mainRoute := v1alpha3.HTTPRoute{
+		Route: []*v1alpha3.HTTPRouteDestination{
 			{
-				Route: []*v1alpha3.HTTPRouteDestination{
-					{
-						Destination: &v1alpha3.Destination{
-							Host:   "voting-app-ui",
-							Subset: "v1",
-						},
-						Weight: 100,
-					},
+				Destination: &v1alpha3.Destination{
+					Host:   serviceSpec.Name,
+					Subset: serviceSpec.Version,
 				},
-				Mirror: &v1alpha3.Destination{
-					Host:   "voting-app-ui",
-					Subset: "v2",
-				},
-				MirrorPercentage: &v1alpha3.Percent{
-					Value: 10,
-				},
+				Weight: 100,
 			},
 		},
-	},
+	}
+
+	if traffic.HasMirroring {
+		mainRoute.Mirror = &v1alpha3.Destination{
+			Host:   serviceSpec.Name,
+			Subset: traffic.MirrorToVersion,
+		}
+		mainRoute.MirrorPercentage = &v1alpha3.Percent{
+			Value: 10,
+		}
+
+	}
+
+	return istioclient.VirtualService{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "networking.istio.io/v1alpha3",
+			Kind:       "VirtualService",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceSpec.Name,
+			Namespace: nameNamespaceSpec.Name,
+		},
+		Spec: v1alpha3.VirtualService{
+			Hosts: []string{
+				traffic.ExternalHostname,
+			},
+			Gateways: []string{
+				traffic.GatewayName,
+			},
+			Http: []*v1alpha3.HTTPRoute{&mainRoute},
+		},
+	}
 }
 
-// Define the DestinationRule
-var votingAppDestinationRule = &istioclient.DestinationRule{
-	TypeMeta: metav1.TypeMeta{
-		APIVersion: "networking.istio.io/v1alpha3",
-		Kind:       "DestinationRule",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "voting-app-ui",
-		Namespace: "voting-app",
-		Labels: map[string]string{
-			"type": "dev",
-		},
-	},
-	Spec: v1alpha3.DestinationRule{
-		Host: "voting-app-ui",
-		Subsets: []*v1alpha3.Subset{
-			{
-				Name: "v1",
-				Labels: map[string]string{
-					"version": "v1",
-				},
-			},
-			{
-				Name: "v2",
-				Labels: map[string]string{
-					"version": "v2",
-				},
+func FrontendDestinationRule(serviceSpec ServiceSpec, nameNamespaceSpec NamespaceSpec, traffic Traffic) istioclient.DestinationRule {
+	subsets := []*v1alpha3.Subset{
+		{
+			Name: "v1",
+			Labels: map[string]string{
+				"version": "v1",
 			},
 		},
-	},
+	}
+
+	if traffic.HasMirroring {
+		devSubset := v1alpha3.Subset{
+			Name: traffic.MirrorToVersion,
+			Labels: map[string]string{
+				"version": traffic.MirrorToVersion,
+			},
+		}
+		subsets = append(subsets, &devSubset)
+	}
+	return istioclient.DestinationRule{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "networking.istio.io/v1alpha3",
+			Kind:       "DestinationRule",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceSpec.Name,
+			Namespace: nameNamespaceSpec.Name,
+		},
+		Spec: v1alpha3.DestinationRule{
+			Host:    serviceSpec.Name,
+			Subsets: subsets,
+		},
+	}
 }
 
 // Helper function to create int32 pointers
--- kloud-kontrol/go.mod
+++ kloud-kontrol/go.mod
@@ -5,13 +5,19 @@ go 1.22.0
 toolchain go1.22.3
 
 require (
+	github.com/compose-spec/compose-go v1.20.2
 	github.com/labstack/echo/v4 v4.12.0
+	github.com/samber/lo v1.39.0
+	istio.io/api v1.22.1-0.20240524024004-b6815be0740d
 	istio.io/client-go v1.22.1
 	k8s.io/api v0.30.2
 	k8s.io/apimachinery v0.30.2
 )
 
 require (
+	github.com/distribution/reference v0.5.0 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
@@ -20,21 +26,28 @@ require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	istio.io/api v1.22.1-0.20240524024004-b6815be0740d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
