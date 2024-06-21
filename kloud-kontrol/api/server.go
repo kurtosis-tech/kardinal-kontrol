6132540459d29c31e49648897b5e76d4e1591bb6
lostbean <talktoedgar@gmail.com>
2024-06-21 17:02:56 -0300

    split into dev and prod functions

--- kloud-kontrol/api/server.go
+++ kloud-kontrol/api/server.go
@@ -10,6 +10,7 @@ import (
 
 	compose "github.com/compose-spec/compose-go/types"
 	"gopkg.in/yaml.v2"
+	"k8s.io/client-go/rest"
 	"kardinal.kloud-kontrol/engine"
 	"kardinal.kloud-kontrol/engine/template"
 	"kardinal.kloud-kontrol/types"
@@ -41,9 +42,24 @@ func NewStrictHandler(si api.StrictServerInterface) api.ServerInterface {
 func (Server) PostDevFlow(ctx context.Context, request api.PostDevFlowRequestObject) (api.PostDevFlowResponseObject, error) {
 	serviceName := *request.Body.ServiceName
 	imageLocator := *request.Body.ImageLocator
+
+	// TODO: get it from the cli
+	serviceName = "voting-app-ui"
+	imageLocator = "voting-app-ui-v2"
 	log.Printf("Starting new dev flow for service %v on image %v", serviceName, imageLocator)
 
-	project := *request.Body.DockerCompose
+	restConn := engine.ConnectToCluster()
+	// err := applyProdOnlyFlow(restConn, *request.Body.DockerCompose)
+	err := applyProdDevFlow(restConn, *request.Body.DockerCompose, serviceName, imageLocator)
+	if err != nil {
+		return nil, err
+	}
+	resp := "ok"
+	return api.PostDevFlow200JSONResponse(resp), nil
+}
+
+// ============================================================================================================
+func applyProdOnlyFlow(restConn *rest.Config, project []compose.ServiceConfig) error {
 	lo.ForEach(project, func(service compose.ServiceConfig, _ int) {
 		serviceStr, _ := yaml.Marshal(service)
 		fmt.Println(string(serviceStr))
@@ -63,7 +79,7 @@ func (Server) PostDevFlow(ctx context.Context, request api.PostDevFlowRequestObj
 	frontendService := lo.Filter(serviceSpecs, func(service *types.ServiceSpec, _ int) bool { return service.Name == frontendServiceName })
 	if len(frontendService) == 0 {
 		log.Fatalf("Frontend service not found")
-		return nil, errors.New("Frontend service not found")
+		return errors.New("Frontend service not found")
 	}
 
 	cluster := types.Cluster{
@@ -82,22 +98,20 @@ func (Server) PostDevFlow(ctx context.Context, request api.PostDevFlowRequestObj
 	}
 
 	clusterResources := template.RenderClusterResources(cluster)
-	restConn := engine.ConnectToCluster()
 	engine.ApplyClusterResources(restConn, &clusterResources)
 	engine.CleanUpClusterResources(restConn, &clusterResources)
+	return nil
+}
 
-	// ============================================================================================================
-
-	serviceName = "voting-app-ui"
-	imageLocator = "voting-app-ui-v2"
-
+// ============================================================================================================
+func applyProdDevFlow(restConn *rest.Config, project []compose.ServiceConfig, devServiceName string, devImage string) error {
 	var devServiceSpec types.ServiceSpec
-	devService, found := lo.Find(project, func(service compose.ServiceConfig) bool { return service.ContainerName == serviceName })
+	devService, found := lo.Find(project, func(service compose.ServiceConfig) bool { return service.ContainerName == devServiceName })
 	if !found {
 		log.Fatalf("Frontend service not found")
-		return nil, errors.New("Frontend service not found")
+		return errors.New("Frontend service not found")
 	} else {
-		devService.Image = imageLocator
+		devService.Image = devImage
 		devService.Environment = lo.MapEntries(devService.Environment, func(key string, value *string) (string, *string) {
 			if key == "REDIS" {
 				proxyUrl := "kardinal-db-sidecar"
@@ -151,9 +165,9 @@ func (Server) PostDevFlow(ctx context.Context, request api.PostDevFlowRequestObj
 	allServiceSpecs := append(serviceSpecsDev, &devServiceSpec, &redisProxyOverlay)
 
 	frontendServiceDev := lo.Filter(allServiceSpecs, func(service *types.ServiceSpec, _ int) bool { return service.Name == frontendServiceName })
-	if len(frontendService) == 0 {
+	if len(frontendServiceDev) == 0 {
 		log.Fatalf("Frontend service not found")
-		return nil, errors.New("Frontend service not found")
+		return errors.New("Frontend service not found")
 	}
 
 	clusterDev := types.Cluster{
@@ -171,10 +185,9 @@ func (Server) PostDevFlow(ctx context.Context, request api.PostDevFlowRequestObj
 		Namespace: types.NamespaceSpec{Name: "prod"},
 	}
 
-	cluserDevResources := template.RenderClusterResources(clusterDev)
-	// engine.ApplyClusterResources(&cluserDevResources)
-	fmt.Println(cluserDevResources)
-
-	resp := "ok"
-	return api.PostDevFlow200JSONResponse(resp), nil
+	clusterDevResources := template.RenderClusterResources(clusterDev)
+	engine.ApplyClusterResources(restConn, &clusterDevResources)
+	engine.CleanUpClusterResources(restConn, &clusterDevResources)
+	fmt.Println(clusterDevResources)
+	return nil
 }
