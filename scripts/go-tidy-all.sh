cf0cf7a12631ed23054ac7383b0401d1b1301824
lostbean <talktoedgar@gmail.com>
2024-07-02 12:29:39 -0300

    tidy and update nix toml

--- scripts/go-tidy-all.sh
+++ scripts/go-tidy-all.sh
@@ -4,4 +4,4 @@ set -euo pipefail # Bash "strict mode"
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
 
-find $root_dirpath -type f -name 'go.mod' -exec sh -c 'dir=$(dirname "{}") && cd "$dir" && echo "$dir" && go mod tidy' \;
+find $root_dirpath -type f -name 'go.mod' -exec sh -c 'dir=$(dirname "{}") && cd "$dir" && echo "$dir" && go mod tidy && gomod2nix' \;
