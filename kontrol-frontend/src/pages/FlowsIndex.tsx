daeb5538b92e7f657d1365b321228f12acdeb973
Skylar <skylarmb@gmail.com>
2024-08-22 13:21:45 -0700

    feat: Templates UI part 3 (#18)
    * fix: fix disabled state and form input values
    
    * feat: index view for templates
    
    * feat: select services from the graph
    
    * chore: pin.json
    
    * Update pin.json
    
    * Update pin.json
    
    * Update pin.json
    
    * chore: bun.lockb
    
    * fix: stable hashes (#22)
    
    * dbg: frontend hashes
    
    * dbg: add x64 hash
    
    * dbg: reduce logs
    
    * fix lint
    
    * re-enable relaxed sandbox for testing
    
    * Update pin.json
    
    ---------
    
    Co-authored-by: Edgar Gomes <talktoedgar@gmail.com>

--- kontrol-frontend/src/pages/FlowsIndex.tsx
+++ kontrol-frontend/src/pages/FlowsIndex.tsx
@@ -1,19 +1,41 @@
 import EmptyState from "@/components/EmptyState";
 import PageTitle from "@/components/PageTitle";
-import { useFlowsContext } from "@/contexts/FlowsContext";
-import { Stack } from "@chakra-ui/react";
+import { Spinner, Stack } from "@chakra-ui/react";
 import { FiShield } from "react-icons/fi";
-import Table from "@/components/Table";
+import TemplatesTable from "@/components/TemplatesTable";
+import { useApi } from "@/contexts/ApiContext";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Page = () => {
-  const { flows } = useFlowsContext();
+  const [loading, setLoading] = useState<boolean>(true);
+  const { templates, getTemplates } = useApi();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      await getTemplates();
+    };
+    fetchTemplates();
+    setLoading(false);
+  }, [getTemplates]);
+
+  if (loading) {
+    return <Spinner size="xl" />;
+  }
+
   return (
     <Stack h="100%">
-      <PageTitle title="Org-level Flow Configurations">
+      <PageTitle
+        title="Org-level Flow Configurations"
+        buttonText="New Flow Configuration"
+        onClick={() => {
+          navigate("create");
+        }}
+      >
         See a list of all available flow configurations for your organization
       </PageTitle>
-      {flows.length > 0 ? (
-        <Table />
+      {templates.length > 0 ? (
+        <TemplatesTable templates={templates} />
       ) : (
         <EmptyState
           icon={FiShield}
