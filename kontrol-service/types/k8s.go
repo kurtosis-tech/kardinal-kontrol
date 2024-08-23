93ff08aed00d06a51edec73b236f6b52c9b1bb4a
leoporoli <leandroporoli@gmail.com>
2024-08-23 14:26:01 -0300

    feat: adding the cluster resources manifest endpoint (#23)
    * adding the cluster resources manifest endpoint
    
    * gomod2nix execution
    
    * swith the manifest endpoint from the manager api to cli api
    
    * ping dependencies from main branch
    
    * gomod2nix execution

--- kontrol-service/types/k8s.go
+++ kontrol-service/types/k8s.go
@@ -5,6 +5,14 @@ import (
 	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	namespaceApiVersion = "v1"
+	namespaceKind       = "Namespace"
+	istioLabel          = "istio-injection"
+	enabledIstioValue   = "enabled"
 )
 
 type ClusterResources struct {
@@ -15,4 +23,19 @@ type ClusterResources struct {
 	Gateway               v1alpha3.Gateway                      `json:"gateway"`
 	EnvoyFilters          []v1alpha3.EnvoyFilter                `json:"envoy_filters"`
 	AuthorizationPolicies []securityv1beta1.AuthorizationPolicy `json:"authorization_policies"`
+}
+
+func NewNamespaceWithIstioEnabled(namespaceName string) *corev1.Namespace {
+	return &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: namespaceApiVersion,
+			Kind:       namespaceKind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespaceName,
+			Labels: map[string]string{
+				istioLabel: enabledIstioValue,
+			},
+		},
+	}
 }
