66a497e61e1b1d5fa36b32a505139fecd98def43
lostbean <talktoedgar@gmail.com>
2024-09-23 09:37:35 -0300

    add kiali to dev minikube script

--- scripts/start-minikube.sh
+++ scripts/start-minikube.sh
@@ -10,4 +10,10 @@ minikube addons enable metrics-server
 
 istioctl install --set profile=minimal --set meshConfig.accessLogFile=/dev/stdout -y
 
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/jaeger.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/kiali.yaml
+kubectl rollout status deployment/kiali -n istio-system
+
 minikube dashboard
