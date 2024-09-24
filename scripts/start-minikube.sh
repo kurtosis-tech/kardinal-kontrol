063ce882226421b980fcd1ccb96ba7fc3a023769
lostbean <talktoedgar@gmail.com>
2024-09-24 16:44:29 -0300

    install gateway api

--- scripts/start-minikube.sh
+++ scripts/start-minikube.sh
@@ -10,6 +10,8 @@ minikube addons enable metrics-server
 
 istioctl install --set profile=minimal --set meshConfig.accessLogFile=/dev/stdout -y
 
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/prometheus.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/grafana.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.10/samples/addons/jaeger.yaml
