36de2173e83f3a098df09816166782f6c303bfc4
Tedi Mitiku <tedi.m52@gmail.com>
2024-10-08 23:12:57 +0900

    revert main

--- kontrol-service/main.go
+++ kontrol-service/main.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"os"
 	"runtime/debug"
+	"strconv"
 )
 
 func main() {
@@ -34,27 +35,23 @@ func main() {
 }
 
 func startServer(isDevMode bool) {
-	//dbHostname := os.Getenv("DB_HOSTNAME")
-	dbHostname := "localhost"
-	//dbUsername := os.Getenv("DB_USERNAME")
-	dbUsername := "postgres"
-	//dbPassword := os.Getenv("DB_PASSWORD")
-	dbPassword := "MyPassword1!"
-	//dbName := os.Getenv("DB_NAME")
-	//if dbHostname == "" || dbUsername == "" || dbPassword == "" || dbName == "" {
-	//	logrus.Fatal("One of the following environment variables is not set: DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME")
-	//}
-	dbName := "postgres"
-	//dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
-	//if err != nil {
-	//	logrus.Fatal("An error occurred parsing the DB port number", err)
-	//}
+	dbHostname := os.Getenv("DB_HOSTNAME")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	if dbHostname == "" || dbUsername == "" || dbPassword == "" || dbName == "" {
+		logrus.Fatal("One of the following environment variables is not set: DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME")
+	}
+	dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		logrus.Fatal("An error occurred parsing the DB port number", err)
+	}
 
 	dbConnectionInfo, err := database.NewDatabaseConnectionInfo(
 		dbUsername,
 		dbPassword,
 		dbHostname,
-		uint16(51940),
+		uint16(dbPort),
 		dbName,
 	)
 	if err != nil {
